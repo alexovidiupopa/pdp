32ee8baf0107c818006c6446451f21db80c79721
Alex <popaalexovidiu@gmail.com>
2021-01-31 17:01:40 +0200

    added exam mpi solution

--- exam-workspace/src/ro/alexpopa/mpi/MpiMatrixPower.java
+++ exam-workspace/src/ro/alexpopa/mpi/MpiMatrixPower.java
@@ -13,11 +13,32 @@ public class MpiMatrixPower {
         return i * m + j;
     }
 
-    List<Integer> getCellFromIndex(int index, int m) {
+    private static List<Boolean> powers(int n) {
+        int k = 1;
+        List<Boolean> arrays = new ArrayList<>();
+        while (k < n) {
+            if ((n & k) != 0) {
+                arrays.add(true);
+            } else {
+                arrays.add(false);
+            }
+            k = k * 2;
+        }
+        return arrays;
+    }
+
+    private static void killAll(int numberOfProcess) {
+        for (int i = 1; i < numberOfProcess; ++i) {
+            MPI.COMM_WORLD.Send(new int[]{0}, 0, 1, MPI.INT, i, 2);
+        }
+    }
+
+
+    private static List<Integer> getCellFromIndex(int index, int m) {
         return Arrays.asList(index / m, index % m);
     }
 
-    List<Integer> getCellsById(int id, int n, int m, int nrProcesses) {
+    private static List<Integer> getCellsById(int id, int n, int m, int nrProcesses) {
         int chunk = (n * m) / nrProcesses;
         int remaining = (n * m) % nrProcesses;
         int last = (n * m) - remaining;
@@ -32,7 +53,7 @@ public class MpiMatrixPower {
         return cells;
     }
 
-    List<List<Integer>> createEmptyMatrix(int n, int m) {
+    private static List<List<Integer>> createEmptyMatrix(int n, int m) {
         List<List<Integer>> matrix = new ArrayList<>();
         for (int i = 0; i < n; ++i) {
             matrix.add(IntStream.range(0, m).map(value -> 0).boxed().collect(Collectors.toList()));
@@ -40,28 +61,37 @@ public class MpiMatrixPower {
         return matrix;
     }
 
-    Integer computeSingleElement(List<List<Integer>> a, int i, int j, int pow) {
+    private static List<List<Integer>> createIdentityMatrix(int n, int m) {
+        List<List<Integer>> matrix = new ArrayList<>();
+        for (int i = 0; i < n; ++i) {
+            matrix.add(IntStream.range(0, m).map(value -> 0).boxed().collect(Collectors.toList()));
+            matrix.get(i).set(i, 1);
+        }
+        return matrix;
+    }
+
+
+    private static Integer computeSingleElement(List<List<Integer>> a, List<List<Integer>> b, int i, int j) {
+
         int sum = 0;
-        for(int p=1;p<=pow;p++) {
-            for (int k = 0; k < a.size(); ++k) {
-                sum += a.get(i).get(k) * a.get(k).get(j);
-            }
+        for (int k = 0; k < b.size(); ++k) {
+            sum += a.get(i).get(k) * b.get(k).get(j);
         }
         return sum;
     }
 
-    List<List<Integer>> work(int n, int m, List<List<Integer>> a, List<Integer> cells, int pow) {
+    private static List<List<Integer>> work(int n, int m, List<List<Integer>> a, List<List<Integer>> b, List<Integer> cells) {
         List<List<Integer>> matrix = createEmptyMatrix(n, m);
         cells.stream()
                 .map(cell -> getCellFromIndex(cell, m))
                 .forEach(cell -> {
                     int i = cell.get(0), j = cell.get(1);
-                    matrix.get(i).set(j, computeSingleElement(a, i, j, pow));
+                    matrix.get(i).set(j, computeSingleElement(a, b, i, j));
                 });
         return matrix;
     }
 
-    void unitePartialSolution(List<List<Integer>> solution, List<List<Integer>> temp) {
+    private static void unitePartialSolution(List<List<Integer>> solution, List<List<Integer>> temp) {
         int n = solution.size(), m = solution.get(0).size();
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
@@ -71,70 +101,97 @@ public class MpiMatrixPower {
         }
     }
 
-    void master(int nrProcesses, List<List<Integer>> a, int pow) {
+
+    private static void master(int nrProcesses, List<List<Integer>> a, int k) {
         int n = a.size(), m = a.get(0).size();
         List<Integer> masterCells = getCellsById(0, n, m, nrProcesses);
-        List<List<Integer>> solution = createEmptyMatrix(n, m);
+        List<List<Integer>> resulting = createIdentityMatrix(n, m);
+        List<Boolean> partOfResult = powers(k);
+        List<List<Integer>> solution;
 
-        for (int i = 1; i < nrProcesses; ++i) {
-            List<Integer> childCells = getCellsById(i, n, m, nrProcesses);
-            MPI.COMM_WORLD.Send(new int[]{pow}, 0, 1, MPI.INT, i, 0);
-            MPI.COMM_WORLD.Send(new Object[]{a}, 0, 1, MPI.OBJECT, i, 0);
-            MPI.COMM_WORLD.Send(new Object[]{a}, 0, 1, MPI.OBJECT, i, 0);
-            MPI.COMM_WORLD.Send(new Object[]{childCells}, 0, 1, MPI.OBJECT, i, 0);
+        System.out.println(partOfResult);
+        for (Boolean result : partOfResult) {
+            if (result) {
+                solution = createEmptyMatrix(n, m);
+                for (int i = 1; i < nrProcesses; ++i) {
+                    List<Integer> childCells = getCellsById(i, n, m, nrProcesses);
+                    MPI.COMM_WORLD.Send(new int[]{1}, 0, 1, MPI.INT, i, 2);
+                    MPI.COMM_WORLD.Send(new Object[]{new ArrayList<>(resulting)}, 0, 1, MPI.OBJECT, i, 0);
+                    MPI.COMM_WORLD.Send(new Object[]{new ArrayList<>(a)}, 0, 1, MPI.OBJECT, i, 0);
+                    MPI.COMM_WORLD.Send(new Object[]{childCells}, 0, 1, MPI.OBJECT, i, 0);
+                }
+
+                List<List<Integer>> parting = work(n, m, resulting, a, masterCells);
+                unitePartialSolution(solution, parting);
+                for (int i = 1; i < nrProcesses; ++i) {
+                    Object[] temp = new Object[1];
+                    MPI.COMM_WORLD.Recv(temp, 0, 1, MPI.OBJECT, i, 1);
+                    List<List<Integer>> childResult = (List<List<Integer>>) temp[0];
+                    unitePartialSolution(solution, childResult);
+                }
+                resulting = solution;
+                System.out.printf("Needed to multiply with answer: %s%n", resulting);
+            }
+            for (int i = 1; i < nrProcesses; ++i) {
+                List<Integer> childCells = getCellsById(i, n, m, nrProcesses);
+                MPI.COMM_WORLD.Send(new int[]{1}, 0, 1, MPI.INT, i, 2);
+                MPI.COMM_WORLD.Send(new Object[]{new ArrayList<>(a)}, 0, 1, MPI.OBJECT, i, 0);
+                MPI.COMM_WORLD.Send(new Object[]{new ArrayList<>(a)}, 0, 1, MPI.OBJECT, i, 0);
+                MPI.COMM_WORLD.Send(new Object[]{childCells}, 0, 1, MPI.OBJECT, i, 0);
+            }
+            solution = createEmptyMatrix(n, m);
+            List<List<Integer>> part = work(n, m, a, new ArrayList<>(a), masterCells);
+            unitePartialSolution(solution, part);
+            for (int i = 1; i < nrProcesses; ++i) {
+                Object[] temp = new Object[1];
+                MPI.COMM_WORLD.Recv(temp, 0, 1, MPI.OBJECT, i, 1);
+                List<List<Integer>> childResult = (List<List<Integer>>) temp[0];
+                unitePartialSolution(solution, childResult);
+            }
+            a = new ArrayList<>(solution);
+            System.out.printf("Intermediary solution: %s%n", a);
         }
-
-        List<List<Integer>> part = work(n, m, a, masterCells, pow);
-        unitePartialSolution(solution, part);
-        for (int i = 1; i < nrProcesses; ++i) {
-            Object[] temp = new Object[1];
-            MPI.COMM_WORLD.Recv(temp, 0, 1, MPI.OBJECT, i, 1);
-            List<List<Integer>> childResult = (List<List<Integer>>) temp[0];
-            unitePartialSolution(solution, childResult);
-        }
-
-        System.out.println(solution);
+        System.out.printf("Final result: %s%n", resulting);
+        killAll(nrProcesses);
     }
 
-    void worker(int myId, int nrProcesses) {
-        int[] pow = new int[1];
-        Object[] a = new Object[1];
-        Object[] b = new Object[1];
-        Object[] c = new Object[1];
-        MPI.COMM_WORLD.Recv(pow, 0, 1, MPI.INT, 0, 0);
-        MPI.COMM_WORLD.Recv(a, 0, 1, MPI.OBJECT, 0, 0);
-        MPI.COMM_WORLD.Recv(b, 0, 1, MPI.OBJECT, 0, 0);
-        MPI.COMM_WORLD.Recv(c, 0, 1, MPI.OBJECT, 0, 0);
-        List<List<Integer>> matrix1 = (List<List<Integer>>) a[0];
-        List<List<Integer>> matrix2 = (List<List<Integer>>) b[0];
-        List<Integer> cells = (List<Integer>) c[0];
+    private static void worker(int myId, int nrProcesses) {
+        while (true) {
+            Object[] a = new Object[1];
+            Object[] b = new Object[1];
+            Object[] c = new Object[1];
+            int[] alive = new int[1];
+            MPI.COMM_WORLD.Recv(alive, 0, 1, MPI.INT, MPI.ANY_SOURCE, 2);
+            if (alive[0] == 0) {
+                break;
+            }
+            MPI.COMM_WORLD.Recv(a, 0, 1, MPI.OBJECT, 0, 0);
+            MPI.COMM_WORLD.Recv(b, 0, 1, MPI.OBJECT, 0, 0);
+            MPI.COMM_WORLD.Recv(c, 0, 1, MPI.OBJECT, 0, 0);
+            List<List<Integer>> matrix1 = (List<List<Integer>>) a[0];
+            List<List<Integer>> matrix2 = (List<List<Integer>>) b[0];
+            List<Integer> cells = (List<Integer>) c[0];
 
-        int n = matrix1.size(), m = matrix2.get(0).size();
-        List<List<Integer>> part = work(n, m, matrix1, cells, pow[0]);
-        MPI.COMM_WORLD.Send(new Object[]{part}, 0, 1, MPI.OBJECT, 0, 1);
-    }
-
-    public void run(String[] args)
-    {
-        MPI.Init(args);
-        int currentIndex = MPI.COMM_WORLD.Rank();
-        int clusterSize = MPI.COMM_WORLD.Size();
-
-        List<List<Integer>> a = Arrays.asList(Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3));
-        int pow = 3;
-
-        if (currentIndex == 0)
-        {
-            master(clusterSize, a, pow);
+            int n = matrix1.size(), m = matrix2.get(0).size();
+            List<List<Integer>> part = work(n, m, matrix1, matrix2, cells);
+            MPI.COMM_WORLD.Send(new Object[]{part}, 0, 1, MPI.OBJECT, 0, 1);
         }
-        else
-        {
-            worker(currentIndex, clusterSize);
-        }
-        MPI.Finalize();
     }
 
     public static void main(String[] args) {
-        new MpiMatrixPower().run(args);
+        MPI.Init(args);
+        int currentIndex = MPI.COMM_WORLD.Rank();
+        int clusterSize = MPI.COMM_WORLD.Size();
+        int n = 5;
+        List<List<Integer>> a = Arrays.asList(Arrays.asList(1, 2), Arrays.asList(1, 2));
+        List<List<Integer>> b = Arrays.asList(Arrays.asList(3, 2), Arrays.asList(3, 2), Arrays.asList(3, 2));
+
+
+        if (currentIndex == 0) {
+            master(clusterSize, a, n);
+        } else {
+            worker(currentIndex, clusterSize);
+        }
+        MPI.Finalize();
     }
 }
--- exam-workspace/src/ro/alexpopa/mpi/MpiMatrixSum.java
+++ exam-workspace/src/ro/alexpopa/mpi/MpiMatrixSum.java
@@ -6,102 +6,137 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
 public class MpiMatrixSum {
 
-    private static void master(List<List<Integer>> a, List<List<Integer>> b, int nrProcs){
-        List<List<Integer>> c = new ArrayList<>();
-        for (int i = 0; i < a.size(); i++) {
-            List<Integer> row = new ArrayList<>();
-            for (int j = 0; j < a.get(0).size(); j++) {
-                row.add(0);
+    public List<List<Integer>> initializeEmptyMatrix(int rows, int cols) {
+        // build a matrix with zeros to be used in computations
+        List<List<Integer>> matrix = new ArrayList<>();
+        for (int i = 0; i < rows; i++) {
+            List<Integer> line = new ArrayList<>();
+            for (int j = 0; j < cols; j++) {
+                line.add(0);
             }
-            c.add(row);
+            matrix.add(line);
         }
-
-        int n = a.size();
-        int toShare = nrProcs - 1;
-        int step = n / toShare;
-        int remainder = n % toShare;
-
-        int start = 0, stop = 0;
-
-        for (int i = 1; i <= toShare ; i++) {
-            stop = start + step;
-            if (remainder>0){
-                stop++;
-                remainder--;
-            }
-
-            int[] metadata = new int[]{start, stop};
-            MPI.COMM_WORLD.Send(metadata, 0, 2, MPI.INT, i, 0);
-            MPI.COMM_WORLD.Send(new Object[]{a},0,1,MPI.OBJECT,i,0);
-            MPI.COMM_WORLD.Send(new Object[]{b},0,1,MPI.OBJECT,i,0);
-
-
-            start = stop;
-        }
-        for (int i = 1; i <= toShare ; i++) {
-            Object[] ans = new Object[1];
-            int[] metadata = new int[2];
-            MPI.COMM_WORLD.Recv(metadata,0,2,MPI.INT,i,0);
-            MPI.COMM_WORLD.Recv(ans, 0, 1, MPI.OBJECT, i, 0);
-            List<List<Integer>> answer = (List<List<Integer>>) ans[0];
-            start = metadata[0];
-            stop = metadata[1];
-            for (int j = start; j < stop; j++) {
-                for (int k = 0; k < answer.get(0).size(); k++) {
-                    c.get(j).set(k, c.get(j).get(k) + answer.get(j).get(k));
-                }
-
-            }
-        }
-
-        System.out.println(c.toString());
+        return matrix;
     }
 
-    private static void worker(int me, int nrProcs){
-        int[] metadata = new int[2];
-        List<List<Integer>> a,b;
-        Object[] objs = new Object[2];
-        MPI.COMM_WORLD.Recv(metadata,0,2,MPI.INT,0,0);
-        MPI.COMM_WORLD.Recv(objs,0,1,MPI.OBJECT,0,0);
-        MPI.COMM_WORLD.Recv(objs,1,1,MPI.OBJECT,0,0);
-        a = (List<List<Integer>>)objs[0];
-        b = (List<List<Integer>>)objs[1];
-
-        List<List<Integer>> c = new ArrayList<>(a);
-        for (int i = 0; i < a.size(); i++) {
-            List<Integer> row = new ArrayList<>();
-            for (int j = 0; j < a.get(0).size(); j++) {
-                row.add(0);
-            }
-            c.add(row);
-        }
-
-        int start = metadata[0], stop = metadata[1];
-        for (int i = start; i < stop; i++) {
-            for (int j = 0; j < a.get(0).size(); j++) {
-                c.get(i).set(j, a.get(i).get(j) + b.get(i).get(j));
-            }
-
-        }
-        MPI.COMM_WORLD.Send(metadata,0,2,MPI.INT, 0,0);
-        MPI.COMM_WORLD.Send(new Object[]{c},0,1,MPI.OBJECT, 0,0);
+    public List<Integer> computeCoordinatesFromIndex(int index, int cols) {
+        return Arrays.asList(index / cols, index % cols);
     }
 
+    public List<List<Integer>> computeWorkForElements(int rows, int cols, List<List<Integer>> a, List<List<Integer>> b, List<Integer> elems) {
+        // for each element, get its coordinates in the form (i,j) and then compute the value for those coordinates, i.e. add a[i][j]+b[i][j]
+        List<List<Integer>> workedMatrix = initializeEmptyMatrix(rows, cols);
+        for (Integer elem : elems) {
+            List<Integer> coordinates = computeCoordinatesFromIndex(elem, cols);
+            int i = coordinates.get(0);
+            int j = coordinates.get(1);
+            workedMatrix.get(i).set(j, a.get(i).get(j) + b.get(i).get(j));
+        }
+        return workedMatrix;
+    }
 
-    public static void main(String[] args) {
+    public List<Integer> getElementsById(int myId, int rows, int cols, int nrProcs) {
+        // function which computes the necessary workload for each process with the id=myId
+        int step = (rows * cols) / nrProcs;
+        int rem = (rows * cols) % nrProcs;
+
+        int extra = (rows * cols) - rem; // extra meaning elements which are unassigned at the moment to any process
+
+        int begin = myId * step;
+        int end = (myId + 1) * step;
+
+        List<Integer> elems = new ArrayList<>();
+        for (int i = begin; i < end; i++) {
+            elems.add(i);
+        }
+
+        if (myId < rem) {   // add remaining elements from the modulo computation in order to ensure an equal amount to each process
+            elems.add(extra + myId);
+        }
+        return elems;
+    }
+
+    public void mergeSolutionsSoFar(List<List<Integer>> solution, List<List<Integer>> partial) {
+        // add the partial result to the solution, i.e. sum each element in solution with each one in partial
+        int rows = solution.size(), cols = solution.get(0).size();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                int prev = solution.get(i).get(j);
+                solution.get(i).set(j, prev + partial.get(i).get(j));
+            }
+        }
+    }
+
+    public List<List<Integer>> master(List<List<Integer>> a, List<List<Integer>> b, int nrProcs) {
+        int rows = a.size(), cols = b.get(0).size();
+        List<List<Integer>> result = initializeEmptyMatrix(rows, cols);
+
+        for (int i = 1; i < nrProcs; i++) { // send work to workers
+            List<Integer> workerElems = getElementsById(i, rows, cols, nrProcs);
+            MPI.COMM_WORLD.Send(new Object[]{workerElems}, 0, 1, MPI.OBJECT, i, 0);
+            MPI.COMM_WORLD.Send(new Object[]{a}, 0, 1, MPI.OBJECT, i, 0);
+            MPI.COMM_WORLD.Send(new Object[]{b}, 0, 1, MPI.OBJECT, i, 0);
+        }
+
+        // let master do his own work
+        List<Integer> masterElements = getElementsById(0, rows, cols, nrProcs);
+        List<List<Integer>> masterWork = computeWorkForElements(rows, cols, a, b, masterElements);
+        mergeSolutionsSoFar(result, masterWork);
+
+        for (int i = 1; i < nrProcs; i++) { // merge worker solutions
+            Object[] fromWorker = new Object[1];
+            MPI.COMM_WORLD.Recv(fromWorker, 0, 1, MPI.OBJECT, i, 0);
+            List<List<Integer>> workerResult = (List<List<Integer>>) fromWorker[0];
+            mergeSolutionsSoFar(result, workerResult);
+        }
+
+        return result;
+    }
+
+    public void worker(int myId, int nrProcs) {
+        Object[] workload = new Object[1];
+        Object[] matrixATemp = new Object[1];
+        Object[] matrixBTemp = new Object[1];
+        MPI.COMM_WORLD.Recv(workload, 0, 1, MPI.OBJECT, 0, 0);
+        MPI.COMM_WORLD.Recv(matrixATemp, 0, 1, MPI.OBJECT, 0, 0);
+        MPI.COMM_WORLD.Recv(matrixBTemp, 0, 1, MPI.OBJECT, 0, 0);
+
+        List<Integer> elems = (List<Integer>) workload[0];
+        List<List<Integer>> a = (List<List<Integer>>) matrixATemp[0];
+        List<List<Integer>> b = (List<List<Integer>>) matrixBTemp[0];
+
+        // compute and send back result
+        int rows = a.size(), cols = b.get(0).size();
+        List<List<Integer>> workerResult = computeWorkForElements(rows, cols, a, b, elems);
+        MPI.COMM_WORLD.Send(new Object[]{workerResult}, 0, 1, MPI.OBJECT, 0, 0);
+    }
+
+    public void run(String[] args)
+    {
         MPI.Init(args);
-        int me = MPI.COMM_WORLD.Rank();
-        int nrProcs = MPI.COMM_WORLD.Size();
-        List<List<Integer>> a = Arrays.asList(Arrays.asList(1,2,3),Arrays.asList(3,4,5), Arrays.asList(4,5,6));
-        List<List<Integer>> b = Arrays.asList(Arrays.asList(1,2,3),Arrays.asList(3,4,5), Arrays.asList(4,5,6));
-        if (me==0){
-            master(a,b,nrProcs);
+        int currentIndex = MPI.COMM_WORLD.Rank();
+        int clusterSize = MPI.COMM_WORLD.Size();
+
+        List<List<Integer>> a = Arrays.asList(Arrays.asList(1, 2, 10), Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3));
+        List<List<Integer>> b = Arrays.asList(Arrays.asList(3, 2,1), Arrays.asList(3, 2,1), Arrays.asList(3, 2,1));
+
+
+        if (currentIndex == 0)
+        {
+            List<List<Integer>> result = master(a, b, clusterSize);
+            System.out.println(result);
         }
-        else{
-            worker(me, nrProcs);
+        else
+        {
+            worker(currentIndex, clusterSize);
         }
         MPI.Finalize();
+    }
+
+    public static void main(String[] args) {
+        new MpiMatrixSum().run(args);
     }
 }
